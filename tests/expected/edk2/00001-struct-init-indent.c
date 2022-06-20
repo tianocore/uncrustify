ecc84525e4b2bedbfbcd338fee126e410dd6a28f
Michael Kubacki <michael.kubacki@microsoft.com>
2022-06-20 19:17:25 -0400

    tests/edk2: Add struct-init-indent unit test
    Adds a unit test that verifies parenthesis within a struct
    initializer format as expected against the edk2 configuration.

---
+++ tests/expected/edk2/00001-struct-init-indent.c
@@ -0,0 +1,21 @@
+/* Processor information */
+STATIC SMBIOS_TYPE4  mSmbiosType4 = {
+  {
+    SocketTypeBase,                                           // Socket type
+    ProcessorVersionBase,                                     // Processor version, update dynamically
+    (                                                         // Status
+      (1 << 6) |                                              // CPU Socket Populated
+      (1 << 0)                                                // CPU Enabled
+    ),
+    ProcessorUpgradeOther,                                    // Processor Upgrade
+    (                                                         // Processor characteristics
+      (1 << 2) |                                              // 64-bit Capable
+      (1 << 3) |                                              // Multi-Core
+      (1 << 5) |                                              // Execute Protection
+      (1 << 6) |                                              // Enhanced Virtualization
+      (1 << 7)                                                // Power/Performance Control
+    ),
+    ProcessorFamily       // Processor Family
+  },
+  TYPE4_STRINGS
+};
